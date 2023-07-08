49fc0009ff6a33ed2cbefb9129f68087cae5e266
vlad1slove1 <vlad5114@gmail.com>
2023-07-08 19:40:15 +0300

    update App

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-toastify/dist/ReactToastify.css'
+
 import React, { useEffect } from 'react'
 import TagManager from 'react-gtm-module'
 import { AppInsightsContext } from '@microsoft/applicationinsights-react-js'
 import { reactPlugin } from '../lib/appinsights'
+import { ToastContainer } from 'react-toastify'
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -13,11 +17,18 @@ function MyApp({ Component, pageProps }) {
   const TEST = process.env.NEXT_PUBLIC_TESTING_MODE
 
   if (TEST === 'on') {
-    return <Component {...pageProps} />
+    return (
+      <>
+        <Component {...pageProps} />
+        <ToastContainer />
+      </>
+    )
   }
+
   return (
     <AppInsightsContext.Provider value={reactPlugin}>
       <Component {...pageProps} />
+      <ToastContainer />
     </AppInsightsContext.Provider>
   )
 }
