9db2d3c964aa64e6b5808508598cbdae4abb8cff
vlad1slove1 <vlad5114@gmail.com>
2023-07-08 19:43:14 +0300

    create experinces / prices / specs lists

---
+++ src/components/formComponents/pricesSelect.js
@@ -0,0 +1,12 @@
+export default {
+  defaultOption: 'Бесплатно',
+  options: [
+    { id: 0, price: 'Бесплатно' },
+    { id: 1, price: '1 000' },
+    { id: 2, price: '3 000' },
+    { id: 3, price: '5 000' },
+    { id: 4, price: '7 000' },
+    { id: 5, price: '10 000' },
+    { id: 6, price: 'По договоренности' },
+  ],
+}
