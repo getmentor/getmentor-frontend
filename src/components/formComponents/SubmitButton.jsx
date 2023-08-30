eb727fd594b1d7fb214c5f8cf3f3a73c94193914
vlad1slove1 <vlad5114@gmail.com>
2023-08-30 20:59:07 +0300

    fixed SubmitButton with .css styles

--- src/components/formComponents/SubmitButton.jsx
+++ src/components/formComponents/SubmitButton.jsx
@@ -1,34 +1,14 @@
-import React, { useState } from 'react';
+import styles from './SubmitButtonStyles.module.css';
+
+import React from 'react';
 import { Button } from 'react-bootstrap';
 
 export default function SubmitButton({ text }) {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
-
-  const buttonStyle = {
-    backgroundColor: isHovered ? "#ff6a3d" : "#fff",
-    color: isHovered ? "#000" : "#0d6efd",
-    padding: "10px 20px",
-    borderRadius: "5px",
-    transition: "all 0.2s ease-in-out",
-    transform: isHovered ? "scale(1.1)" : "scale(1)",
-  };
-
   return (
     <Button
       type="submit"
       size="lg"
-      className="border-0 rounded-pill"
-      style={buttonStyle}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
+      className={`${styles.submitButton} border-0 rounded-pill`}
     >
       {text}
     </Button>
