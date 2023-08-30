07da38d120322f5acb3cfc57eb6c1649bb419b9f
vlad1slove1 <vlad5114@gmail.com>
2023-08-30 22:55:00 +0300

    created i18n.js instance file

---
+++ src/pages/i18n.js
@@ -0,0 +1,20 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+import Backend from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+i18n
+  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+  .use(Backend)
+  // detect user language
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  .init({
+    fallbackLng: 'ru',
+    debug: true,
+  })
+
+export default i18n
