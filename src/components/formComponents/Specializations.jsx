d11913e0e08a35417372454fd00fc36175bbe213
vlad1slove1 <vlad5114@gmail.com>
2023-10-01 21:29:41 +0300

    updated Specializations component

--- src/components/formComponents/Specializations.jsx
+++ src/components/formComponents/Specializations.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, ListGroup, Button } from 'react-bootstrap';
+import axios from 'axios';
+import routes from '../../server/routes';
 
+import { Spinner } from '@nextui-org/react';
 import SpecializationsModal from './SpecializationsModal.jsx';
-import specializationList from './specializationList.js';
 import CloseButton from 'react-bootstrap/CloseButton';
 
 import { useTranslation } from 'next-i18next';
@@ -22,10 +24,21 @@ function Tag({ name, onClick }) {
   )
 }
 
+const apiUrl = process.env.NEXT_PUBLIC_MENTORS_BACKEND_URL;
+
 export default function Specializations({ formik, tag }) {
   const [showModal, setShowModal] = useState(false);
   const [tags, setTags] = useState([]);
-  const [specList, setSpecList] = useState(specializationList);
+  const [specList, setSpecList] = useState(null);
+  const [specListCopy, setSpecListCopy] = useState(null);
+
+  useEffect(() => {
+    axios.get(`${apiUrl}${routes.specializationsPath()}`).then((res) => {
+      const specs = res.data.data.specializations;
+      setSpecList(specs);
+      setSpecListCopy(specs);
+    })
+  }, [setSpecList]);
 
   const { t } = useTranslation(['common', 'form', 'formErrors', 'toastify']);
 
@@ -38,7 +51,9 @@ export default function Specializations({ formik, tag }) {
 
     setTags((prevTags) => {
       const newTags = [...prevTags, tagName];
-      setSpecList((prevSpecList) => prevSpecList.filter((spec) => !newTags.includes(spec.name)));
+      const filteredSpecList = specList.filter((spec) => !newTags.includes(spec.name));
+      setSpecListCopy(filteredSpecList.sort((a, b) => a.name > b.name ? 1 : -1));
+
       return newTags;
     });
 
@@ -52,9 +67,9 @@ export default function Specializations({ formik, tag }) {
     setTags((prevTags) => {
       const newTags = prevTags.filter((tag) => tag !== tagName);
 
-      setSpecList((prevSpecList) => {
+      setSpecListCopy((prevSpecList) => {
         const updatedSpecList = [...prevSpecList];
-        const specToAdd = specializationList.find((spec) => spec.name === tagName);
+        const specToAdd = specList.find((spec) => spec.name === tagName);
         if (specToAdd) {
           updatedSpecList.push(specToAdd);
           updatedSpecList.sort((a, b) => a.name > b.name ? 1 : -1);
@@ -66,6 +81,10 @@ export default function Specializations({ formik, tag }) {
       return newTags;
     });
   };
+
+  if (specList === null) {
+    return <Spinner />;
+  }
 
   return (
     <Form.Group>
@@ -79,7 +98,7 @@ export default function Specializations({ formik, tag }) {
         isInvalid={formik.touched.specializations && formik.errors.specializations}
         className={formik.touched.specializations && formik.errors.specializations ? "rounded-0" : "border-0"}
       >
-        <Button variant="light border rounded-0" onClick={handleShowModal} className="mb-3">
+        <Button variant="light border rounded-0" onClick={handleShowModal} className="mb-3" name="spec-add">
           + {t('specialization.specializationButton', { ns: 'form' })}
         </Button>
 
@@ -90,7 +109,7 @@ export default function Specializations({ formik, tag }) {
         showModal={showModal}
         hideModal={handleCloseModal}
         handleAddTag={handleAddTag}
-        specList={specList.sort((a, b) => a.name > b.name ? 1 : -1)}
+        specList={specListCopy}
       />
 
       <Form.Control.Feedback type="invalid" className="invalid-feedback text-center">
